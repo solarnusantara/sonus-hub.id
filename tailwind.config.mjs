6749aec292a7e931e1722f1e0b648c937ce28b24
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-12-15 14:08:12 +0700

    chore: update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,17 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
+    container: {
+      center: true,
       screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
         'max-desktop': '1440px',
-      },
+      }
+    },
+    extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         heading: ['Oswald', 'sans-serif'],
